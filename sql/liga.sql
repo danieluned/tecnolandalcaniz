51cd4321512650ce8504172c56558ecf21624c8c
Daniel Martinez Martinez <danieluned@hotmail.com>
2019-01-09 20:16:56 +0100

    DMM: liga.sql

--- sql/liga.sql
+++ sql/liga.sql
@@ -1,4 +1,3 @@
--- Sistema para almacenar las competiciones 
 
 -- -----------------------------------------------------
 -- Table `competicion`
@@ -29,84 +28,22 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `jornada`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `jornada` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `competicion_id` INT NOT NULL,
-  `fechainicio` DATETIME NULL,
-  `fechafin` DATETIME NULL,
-  `comentario` VARCHAR(200) NULL,
-  PRIMARY KEY (`id`, `competicion_id`),
-  INDEX `fk_jornada_competicion1_idx` (`competicion_id` ASC) ,
-  CONSTRAINT `fk_jornada_competicion1`
-    FOREIGN KEY (`competicion_id`)
-    REFERENCES `competicion` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `partida`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `partida` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `competicion_id` INT NOT NULL,
+  `jornada_id` INT NULL,
   `resultado` VARCHAR(45) NULL,
   `horainicio` DATETIME NULL,
-  `jornada_id` INT NOT NULL,
-  `jornada_competicion_id` INT NOT NULL,
   `comentario` VARCHAR(200) NULL,
   `verificado` TINYINT NULL DEFAULT 0,
   `estado` ENUM('pendiente', 'cerrada', 'jugando', 'disputa') NULL,
   PRIMARY KEY (`id`, `competicion_id`),
   INDEX `fk_partida_competicion_idx` (`competicion_id` ASC) ,
-  INDEX `fk_partida_jornada1_idx` (`jornada_id` ASC, `jornada_competicion_id` ASC) ,
   CONSTRAINT `fk_partida_competicion`
     FOREIGN KEY (`competicion_id`)
     REFERENCES `competicion` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_partida_jornada1`
-    FOREIGN KEY (`jornada_id` , `jornada_competicion_id`)
-    REFERENCES `jornada` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `equipoinscrito`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `equipoinscrito` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `competicion_id` INT NOT NULL,
-  `capitan` INT NULL,
-  `nombre` VARCHAR(200) NULL,
-  `logotipo` VARCHAR(200) NULL,
-  `info` VARCHAR(200) NULL,
-  `fecha` DATETIME NULL,
-  PRIMARY KEY (`id`, `competicion_id`),
-  INDEX `fk_equipo_competicion1_idx` (`competicion_id` ASC) ,
-  INDEX `fk_equipo_capitan_idx` (`capitan` ASC) ,
-  CONSTRAINT `fk_equipo_competicion1`
-    FOREIGN KEY (`competicion_id`)
-    REFERENCES `competicion` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_equipo_capitan`
-    FOREIGN KEY (`capitan`)
-    REFERENCES `jugadorinscrito` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -116,32 +53,40 @@ ENGINE = InnoDB;
 -- Table `jugadorinscrito`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jugadorinscrito` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `competicion_id` INT NOT NULL,
-  `id` INT NOT NULL,
-  `equipo_id` INT NULL,
-  `equipo_competicion_id` INT NULL,
   `nombre` VARCHAR(200) NULL,
+  `equipoinscrito_id` INT NULL,
+  `users_id` INT NULL,
   `logotipo` VARCHAR(200) NULL,
   `info` VARCHAR(200) NULL,
-  `usuario_id` INT NOT NULL,
   `fecha` DATETIME NULL,
-  PRIMARY KEY (`competicion_id`, `id`),
+  PRIMARY KEY (`id`, `competicion_id`),
   INDEX `fk_competicion_has_usuario_competicion1_idx` (`competicion_id` ASC) ,
-  INDEX `fk_inscrito_equipo1_idx` (`equipo_id` ASC, `equipo_competicion_id` ASC) ,
-  INDEX `fk_jugadorinscrito_usuario1_idx` (`usuario_id` ASC) ,
   CONSTRAINT `fk_competicion_has_usuario_competicion1`
     FOREIGN KEY (`competicion_id`)
     REFERENCES `competicion` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_inscrito_equipo1`
-    FOREIGN KEY (`equipo_id` , `equipo_competicion_id`)
-    REFERENCES `equipoinscrito` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_jugadorinscrito_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `usuario` (`id`)
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `equipoinscrito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `equipoinscrito` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `competicion_id` INT NOT NULL,
+  `nombre` VARCHAR(200) NULL,
+  `logotipo` VARCHAR(200) NULL,
+  `info` VARCHAR(200) NULL,
+  `fecha` DATETIME NULL,
+  `capitan` INT NULL,
+  PRIMARY KEY (`id`, `competicion_id`),
+  INDEX `fk_equipo_competicion1_idx` (`competicion_id` ASC) ,
+  CONSTRAINT `fk_equipo_competicion1`
+    FOREIGN KEY (`competicion_id`)
+    REFERENCES `competicion` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -152,24 +97,18 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `subecontenido` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `partida_competicion_id` INT NOT NULL,
+  `competicion_id` INT NOT NULL,
   `partida_id` INT NOT NULL,
-  `usuario_id` INT NOT NULL,
+  `users_id` INT NOT NULL,
   `filename` VARCHAR(200) NULL,
   `tipo` VARCHAR(45) NULL,
   `comentario` VARCHAR(200) NULL,
   `fecha` DATETIME NULL,
-  PRIMARY KEY (`id`, `partida_competicion_id`, `partida_id`, `usuario_id`),
-  INDEX `fk_contenidomultimedia_partida1_idx` (`partida_id` ASC, `partida_competicion_id` ASC) ,
-  INDEX `fk_multimedia_usuario1_idx` (`usuario_id` ASC) ,
+  PRIMARY KEY (`id`, `competicion_id`, `partida_id`, `users_id`),
+  INDEX `fk_contenidomultimedia_partida1_idx` (`partida_id` ASC, `competicion_id` ASC) ,
   CONSTRAINT `fk_contenidomultimedia_partida1`
-    FOREIGN KEY (`partida_id` , `partida_competicion_id`)
+    FOREIGN KEY (`partida_id` , `competicion_id`)
     REFERENCES `partida` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_multimedia_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `usuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -180,22 +119,16 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `comenta` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `competicion_id` INT NOT NULL,
   `partida_id` INT NOT NULL,
-  `partida_competicion_id` INT NOT NULL,
-  `usuario_id` INT NOT NULL,
+  `users_id` INT NOT NULL,
   `texto` TEXT NULL,
   `fecha` DATETIME NULL,
-  PRIMARY KEY (`id`, `partida_id`, `partida_competicion_id`, `usuario_id`),
-  INDEX `fk_partida_has_usuario_usuario1_idx` (`usuario_id` ASC) ,
-  INDEX `fk_partida_has_usuario_partida1_idx` (`partida_id` ASC, `partida_competicion_id` ASC) ,
+  PRIMARY KEY (`id`, `competicion_id`, `partida_id`, `users_id`),
+  INDEX `fk_partida_has_usuario_partida1_idx` (`partida_id` ASC, `competicion_id` ASC) ,
   CONSTRAINT `fk_partida_has_usuario_partida1`
-    FOREIGN KEY (`partida_id` , `partida_competicion_id`)
+    FOREIGN KEY (`partida_id` , `competicion_id`)
     REFERENCES `partida` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_partida_has_usuario_usuario1`
-    FOREIGN KEY (`usuario_id`)
-    REFERENCES `usuario` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -220,29 +153,35 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `jornada`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `jornada` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `competicion_id` INT NOT NULL,
+  `fechainicio` DATETIME NULL,
+  `fechafin` DATETIME NULL,
+  `comentario` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`, `competicion_id`),
+  INDEX `fk_jornada_competicion1_idx` (`competicion_id` ASC) ,
+  CONSTRAINT `fk_jornada_competicion1`
+    FOREIGN KEY (`competicion_id`)
+    REFERENCES `competicion` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `participan`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `participan` (
+  `competicion_id` INT NOT NULL,
   `partida_id` INT NOT NULL,
-  `partida_competicion_id` INT NOT NULL,
   `equipoinscrito_id` INT NOT NULL,
-  `equipoinscrito_competicion_id` INT NOT NULL,
   `presentado` TINYINT NULL,
   `puntuacion` DOUBLE NULL,
   `conforme` ENUM('conforme', 'noconforme', 'sincontestar') NULL DEFAULT 'sincontestar',
-  PRIMARY KEY (`partida_id`, `partida_competicion_id`, `equipoinscrito_id`, `equipoinscrito_competicion_id`),
-  INDEX `fk_partida_has_equipoinscrito_equipoinscrito1_idx` (`equipoinscrito_id` ASC, `equipoinscrito_competicion_id` ASC) ,
-  INDEX `fk_partida_has_equipoinscrito_partida1_idx` (`partida_id` ASC, `partida_competicion_id` ASC) ,
-  CONSTRAINT `fk_partida_has_equipoinscrito_partida1`
-    FOREIGN KEY (`partida_id` , `partida_competicion_id`)
-    REFERENCES `partida` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_partida_has_equipoinscrito_equipoinscrito1`
-    FOREIGN KEY (`equipoinscrito_id` , `equipoinscrito_competicion_id`)
-    REFERENCES `equipoinscrito` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`competicion_id`, `partida_id`, `equipoinscrito_id`))
 ENGINE = InnoDB;
 
 
@@ -250,31 +189,12 @@ ENGINE = InnoDB;
 -- Table `juega`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `juega` (
-  `jugadorinscrito_competicion_id` INT NOT NULL,
-  `jugadorinscrito_id` INT NOT NULL,
+  `competicion_id` INT NOT NULL,
   `partida_id` INT NOT NULL,
-  `partida_competicion_id` INT NOT NULL,
+  `jugadorinscrito_id` INT NOT NULL,
   `puntuacion` DOUBLE NULL,
   `presentado` TINYINT NULL,
   `conforme` ENUM('conforme', 'noconforme', 'sincontestar') NULL DEFAULT 'sincontestar',
-  PRIMARY KEY (`jugadorinscrito_competicion_id`, `partida_id`, `partida_competicion_id`, `jugadorinscrito_id`),
-  INDEX `fk_jugadorinscrito_has_partida_partida1_idx` (`partida_id` ASC, `partida_competicion_id` ASC) ,
-  INDEX `fk_jugadorinscrito_has_partida_jugadorinscrito1_idx` (`jugadorinscrito_competicion_id` ASC) ,
-  INDEX `fk_juega_jugadadorinscrito_idx` (`jugadorinscrito_id` ASC) ,
-  CONSTRAINT `fk_jugadorinscrito_has_partida_jugadorinscrito1`
-    FOREIGN KEY (`jugadorinscrito_competicion_id`)
-    REFERENCES `jugadorinscrito` (`competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_jugadorinscrito_has_partida_partida1`
-    FOREIGN KEY (`partida_id` , `partida_competicion_id`)
-    REFERENCES `partida` (`id` , `competicion_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_juega_jugadadorinscrito`
-    FOREIGN KEY (`jugadorinscrito_id`)
-    REFERENCES `jugadorinscrito` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`competicion_id`, `partida_id`, `jugadorinscrito_id`))
 ENGINE = InnoDB;
 
